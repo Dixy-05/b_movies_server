cff4ab1241e43694d6a2f8b5d5dda98339fe5cff
Dixy-05 <ddbarahona89@gmail.com>
2021-10-01 09:19:52 -0600

    removed testing.js, server update

--- server.js
+++ server.js
@@ -8,9 +8,6 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-app.get('/', (req, res) => {
-  res.send('hey this is working');
-});
 app.use('/api', router);
 
 const PORT = process.env.PORT || 8081;
