aa8220e4064e450313a3a0926ac97c6279f50b2c
Dixy-05 <ddbarahona89@gmail.com>
2021-09-30 23:17:12 -0600

    testing the server

--- server.js
+++ server.js
@@ -8,7 +8,9 @@ require('dotenv').config();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-
+app.get('/', (req, res) => {
+  res.send('hey this is working');
+});
 app.use('/api', router);
 
 const PORT = process.env.PORT || 8081;
