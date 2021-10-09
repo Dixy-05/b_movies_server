9d93956fd5961737aca17782880619ac9e215977
Dixy-05 <ddbarahona89@gmail.com>
2021-10-08 23:22:13 -0600

    new test for userDAO.test.js

--- test/userDAO.test.js
+++ test/userDAO.test.js
@@ -1,18 +1,14 @@
 const userDAO = require('../DAO/usersDAO');
 const db = require('../DB/db');
-jest.mock('../DB/db', () => {
-  return {
-    dbProduction: jest.fn(),
-    dbDevelopment: {},
-  };
-});
+jest.mock('../DB/db');
+let { dbProduction } = require('../DB/db');
 
 const users = [{ id: 1, email: 'test@test.com' }];
 describe('Get user', () => {
   describe('from database', () => {
     it('should call database once', async () => {
-      db.select.mockReturnThis();
-      db.from.mockResolvedValue(users);
+      dbProduction.select.mockReturnThis();
+      dbProduction.from.mockResolvedValue(users);
       const result = await userDAO.getUsers();
       expect(result).toEqual(users);
     });
@@ -25,8 +21,9 @@ describe('Create user', () => {
     password: 'password',
   };
   it('should insert on database', async () => {
-    db = jest.fn().mockResolvedValue({
+    dbProduction.mockReturnValue({
       insert: jest.fn().mockReturnThis(),
+      returning: jest.fn().mockResolvedValue([users[0]]),
     });
     const result = await userDAO.createUser(reqBody);
     expect(result).toEqual(users[0]);
